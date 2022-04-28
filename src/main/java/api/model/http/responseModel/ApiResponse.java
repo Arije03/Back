016f2b5b489a8aabadd4ad70509e5ccecb607553
Arije03 <boubakerarije@gmail.com>
2022-04-28 15:24:20 +0200

    projet makhlouf

--- src/main/java/api/model/http/responseModel/ApiResponse.java
+++ src/main/java/api/model/http/responseModel/ApiResponse.java
@@ -11,12 +11,40 @@ import org.springframework.http.HttpStatus;
  *
  * @author makhlouf
  */
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+
+
 public class ApiResponse {
+	
 	private HttpStatus code;
 	private String message;
 	private Object response;
+	public ApiResponse(HttpStatus code, String message, Object response) {
+		super();
+		this.code = code;
+		this.message = message;
+		this.response = response;
+	}
+	public ApiResponse() {
+		
+	}
+	public HttpStatus getCode() {
+		return code;
+	}
+	public void setCode(HttpStatus code) {
+		this.code = code;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Object getResponse() {
+		return response;
+	}
+	public void setResponse(Object response) {
+		this.response = response;
+	}
+	
+	
 }
